e7eb45488e6e3b4957ebca18e592f3c092606143
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-11-18 15:29:01 +0500

    Update problems.h

--- problems.h
+++ problems.h
@@ -1,17 +1,17 @@
-void days2years(int &days, int &years){
+void days2years(int &days,int &years) {
     years = days / 365;    days = days % 365;
 }
 
-double func(double *x, double *y){
+double func(double *x,double *y) {
     *y = (11*pow(*x, 3))/3 + 5;
     return *y;
 }
 
-int minsNewYear(int *hour, int *min){
+int minsNewYear(int *hour, int *min) {
     return 60*(24-(*hour))-(*min);
 }
 
-double probability(int *y, int *w){
+double probability(int *y,int *w) {
     int cnt = 0;
     for (int i = 1; i <= 6; i++){
         cnt += (i> *y && i> *w || i==*y || i==*w ? 1 : 0);
@@ -19,7 +19,7 @@ double probability(int *y, int *w){
     return double(cnt)/6;
 }
 
-int presses(int *x){
+int presses(int *x) {
     int dgt = *x % 10, n = 0;
     for (int i = 1; i<=dgt; i++){   for (int j = 1; j <= (i == dgt ? to_string(*x).length() : 4); j++){ n += j;}    }
     return n;
