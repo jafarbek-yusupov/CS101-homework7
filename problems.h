8415a8ece0c97b1c32940b3e0b71b57caabed043
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-11-18 14:54:43 +0500

    Create problems.h

---
+++ problems.h
@@ -0,0 +1,23 @@
+void days2years(int &days, int &years) {
+   
+}
+// Problem 2
+double func(double *x,double *y) {
+    
+}
+
+
+// Problem 3
+int minsNewYear(int *hour, int *min) {
+    
+}
+
+// Problem 4
+double probability(int *y,int *w) {
+   
+}
+
+// Problem 5
+int presses(int *x) {
+    
+}
